6d687c53eff4282bb5d88df1355821436e14b373
Ruslan Symonenko <symonenkors@gmail.com>
2023-10-18 19:51:20 +0300

    fixed bug with renaming script file, and multiply renamings of files

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import os
 import datetime
 import sys
-
-# DIRECTORY = r"C:\Users\Lenovo\OneDrive\Рабочий стол\it\python\projects\files-renamer\test"
+import re
 
 
 def get_valid_name(file_name, folder_name):
     current_date = datetime.date.today().strftime("%d-%m-%Y")
     name, extension = os.path.splitext(file_name)
 
-    valid_name = f"{folder_name}_{name}_{current_date}{extension}"
+    date_pattern = r"\d{2}-\d{2}-\d{4}"
+    match = re.match(date_pattern, file_name)
+
+    if match:
+        valid_name = file_name
+    else:
+        valid_name = f"{current_date}_{folder_name}_{name}{extension}"
+
     return valid_name
 
 
-def rename_file(root, file_name):
+def rename_file(root, file_name, script_name):
     folder_name = os.path.basename(root)
-    valid_name = get_valid_name(file_name, folder_name)
-    old_file = os.path.join(root, file_name)
-    new_file = os.path.join(root, valid_name)
 
-    os.rename(old_file, new_file)
+    if file_name != script_name:
+        valid_name = get_valid_name(file_name, folder_name)
+        old_file = os.path.join(root, file_name)
+        new_file = os.path.join(root, valid_name)
+
+        os.rename(old_file, new_file)
 
 
-def rename_files(directory):
+def rename_files(directory, script_name):
     for root, dirs, files in os.walk(directory):
         for file_name in files:
-            rename_file(root, file_name)
+            rename_file(root, file_name, script_name)
 
 
 def main():
     script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    script_name = os.path.basename(sys.argv[0])
     DIRECTORY = script_dir
-    rename_files(DIRECTORY)
+    # DIRECTORY = r"C:\Users\Lenovo\OneDrive\Рабочий стол\it\python\projects\files-renamer\test"
+    rename_files(DIRECTORY, script_name)
 
 
 if __name__ == "__main__":
